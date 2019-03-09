1a91afc2b7c1714e1573d5db60c29141e2268d62
KY <infcurious@gmail.com>
2019-03-09 22:16:59 +0800

    Not quite work yet

---
+++ ap.py
@@ -0,0 +1,254 @@
+from colorama import init, Fore
+init()
+
+class Colorize:
+    """
+    Make string colorful.
+    Use Coloroma.
+
+    Supported constants/colors:
+    - Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+    - Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+    - Style: DIM, NORMAL, BRIGHT, RESET_ALL
+    """
+
+    # TODO:
+    # - Make this configurable
+    # - Exhaustive
+    COLOR_BY_TYPE = {
+        'str': Fore.GREEN,
+        'list': Fore.BLUE,
+        'tuple': Fore.YELLOW,
+    }
+
+    def __init__(self):
+        raise "This is a mixin. Can't be initialized."
+
+    def colorize(self, str, type):
+        """
+        Return a string with color info
+
+        :param str str: The string to be colorized
+        :param str type: The type of what the str originally is, use type(obj).__name__
+        """
+        color = Colorize.COLOR_BY_TYPE.get(type, Fore.RESET)
+        return '{0}{1}'.format(color, str)
+
+
+class Indentator:
+    """
+    Keep track of indentation
+    """
+    def __init__(self, indent_width = 4):
+        self.indent_width = indent_width
+        self.indentation = indent_width
+
+    def increase_indentation(self, func):
+        self.indentation += self.indent_width
+        print("**increase indentation**")
+        result = func()
+        self.indentation -= self.indent_width
+        print("**decrease indentation**")
+        return result
+
+# indentator = Indentator(4)
+# print('*' * indentator.indentation)
+# def foo():
+#     print('hello world')
+# indentator.increase_indentation(foo)
+# print('*' * indentator.indentation)
+
+class BaseFormatter(Colorize):
+    def __init__(self):
+        pass
+
+    def align(self, value, width):
+        return value.rjust(width)
+
+class SimpleFormatter(BaseFormatter):
+    def __init__(self, object, type):
+        self.object = object
+        self.type = type
+
+    def format(self):
+        out = self.object
+        if isinstance(self.object, str):
+            out = '\"{}\"'.format(out)
+        return self.colorize(out, self.type)
+
+# objs = [
+#     'this is a string',
+#     {'foo': 'bar'},
+#     (1, 2),
+#     ['hello', 'world']
+# ]
+# for o in objs:
+#     sf = SimpleFormatter(o, type(o).__name__)
+#     print(sf.format())
+
+class Formatter:
+    """
+    Format object according to their type.
+    """
+    def __init__(self, inspector):
+        self.inspector = inspector
+
+    def format(self, object, type = None):
+        """
+        Entry point to format an object.
+        """
+        if isinstance(object, dict):
+            return self.format_dict(object)
+        else:
+            return self.format_str(object)
+
+    def format_str(self, object):
+        return SimpleFormatter(object, type(object).__name__).format()
+
+    def format_dict(self, object):
+        return DictFormatter(object, self.inspector).format()
+
+
+class Inspector:
+    def __init__(self):
+        self.formatter = Formatter(self)
+        self.indentator = Indentator()
+
+    def increase_indentation(self, func):
+        return self.indentator.increase_indentation(func)
+
+    def indentation(self):
+        return self.indentator.indentation
+
+    def outdent(self):
+        return ' ' * (self.indentator.indentation - self.indentator.indent_width)
+
+    def awesomize(self, object):
+        # if isinstance(object, str):
+        return self.formatter.format(object)
+
+
+class DictFormatter(BaseFormatter):
+    def __init__(self, object, inspector):
+        self.object = object
+        self.inspector = inspector
+
+    def format(self):
+        if len(self.object) == 0:
+            return '{}'
+        else:
+            return self.awesome_dict()
+
+    def awesome_dict(self):
+        """
+        return an awesome dict.
+        """
+        dict = self.printable_dict()
+        return self.colorize('{\n', None) + \
+            ',\n'.join(dict) + '\n' + \
+            self.inspector.outdent() + \
+            self.colorize('}', None)
+
+    def printable_dict(self):
+        data = self.printable_keys()
+        width = self.left_width(data)
+        return [
+            self.inspector.increase_indentation(
+                lambda: self.build_colorized_dict(datum[0], datum[1], width)
+            ) for datum in data
+        ]
+
+    def build_colorized_dict(self, key, value, width):
+        left_pad_colorized_key = self.align(key, width)
+        colorized_colon = self.colorize(': ', None)
+        awesomized_value = self.inspector.awesomize(value)
+        return '{0}{1}{2}'.format(left_pad_colorized_key, colorized_colon, awesomized_value)
+
+    def printable_keys(self):
+        """
+        return an array of colorized key and raw value pairs
+
+        e.g.
+        given:
+
+        object = {
+            'a': 'va',
+            'c': 'vc',
+            'b': {
+                'd': 'vd'
+            }
+        }
+
+        returns:
+
+        [[colorize('a'), 'va'], [colorize('c'), 'vc'], [colorize('b'), { 'd': 'vd' }]]
+        """
+        keys = self.object.keys()
+        sorted_keys = sorted(keys) # TODO: Make this optional
+        return [[self.colorize(key, None), self.object[key]] for key in sorted_keys]
+
+    def left_width(self, data):
+        """
+        Find the spaces we need to pad on the left side of the dictionary.
+        e.g.
+        {
+            'foo': { ... },
+            'barbarbar': { ... },
+        }
+        we want to align this into
+        {
+                  'foo': { ... },
+            'barbarbar': { ... },
+        }
+        so, the number of spaces we need on the left of the keys is calculated as:
+        (number of indentation * indentation width) + max of keys width
+        {
+        xxxx      'foo': { ... },
+            'barbarbar': { ... },
+        }
+        in this case, left width = (1 * 4) + 9 = 13
+        """
+        max_key_width = max([len(datum[0]) for datum in data])
+        return max_key_width + self.inspector.indentation()
+
+
+def ap(obj):
+    inspector = Inspector()
+    awesome_output = inspector.awesomize(obj)
+    return awesome_output
+
+user = {
+    'email': 'ghopper@gmail.com',
+    'first_name': 'grace',
+    'last_name': 'hopper',
+    'address': {
+        'line_1': '1 Hacker Way',
+        'line_2': '#1-3-3-7',
+        'postal_code': '54321',
+        'city': 'L.A',
+        'state': 'S.F',
+        'country': 'United States of America'
+    },
+    'deeply': {
+        'nested': {
+            'dict': {
+                'that': {
+                    'you': {
+                        'can': {
+                            'never': {
+                                'imagine': [],
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+simple = {
+    'foo': None,
+    'bar': 'bar',
+}
+
+print(ap(user))
