64059c01720b0631e0d37942efa22bb0e0d27195
KY <infcurious@gmail.com>
2019-03-10 10:02:39 +0800

    Support for List formatting

--- ap.py
+++ ap.py
@@ -96,6 +96,8 @@ class Formatter:
         """
         if isinstance(object, dict):
             return self.format_dict(object)
+        elif isinstance(object, list):
+            return self.format_list(object)
         else:
             return self.format_str(object)
 
@@ -105,6 +107,8 @@ class Formatter:
     def format_dict(self, object):
         return DictFormatter(object, self.inspector).format()
 
+    def format_list(self, object):
+        return ListFormatter(object, self.inspector).format()
 
 class Inspector:
     DEFAULT_OPTIONS = {
@@ -122,12 +126,68 @@ class Inspector:
     def indentation(self):
         return self.indentator.indentation
 
+    def indent(self):
+        return ' ' * self.indentator.indentation
+
     def outdent(self):
         return ' ' * (self.indentator.indentation - self.indentator.indent_width)
 
     def awesomize(self, object):
         return self.formatter.format(object)
 
+
+class ListFormatter(BaseFormatter):
+    def __init__(self, object, inspector):
+        self.object = object
+        self.inspector = inspector
+
+    def format(self):
+        if len(self.object) == 0:
+            return '[]'
+        else:
+            return self.awesome_list()
+
+    def awesome_list(self):
+        """
+        return an awesome list.
+        """
+        list = self.printable_list()
+        return self.colorize('[\n', None) + \
+            ',\n'.join(list) + '\n' + \
+            self.inspector.outdent() + \
+            self.colorize(']', None)
+
+    def printable_list(self):
+        data = self.object
+        width = self.left_width(data)
+        return [
+            self.inspector.increase_indentation(
+                lambda: self.build_colorized_list(index, datum, width)
+            ) for index, datum in enumerate(data)
+        ]
+
+    def build_colorized_list(self, index, datum, width):
+        return '{0}{1}'.format(self.list_prefix(index, width), self.inspector.awesomize(datum))
+
+    def left_width(self, data):
+        """
+        return spaces required on the left of the printable list
+
+        e.g.
+        the printable list will be:
+        [
+            [0] 1,
+            [1] 2,
+            [2] 3
+        ]
+        so, the spaces on the left is
+        indentation + length of the index (in string)
+        """
+        return len(str(len(data) - 1))
+
+    def list_prefix(self, index, width):
+        return self.inspector.indent() + \
+            self.colorize('[{0}] '.format(str(index).rjust(width)), 'list')
 
 class DictFormatter(BaseFormatter):
     def __init__(self, object, inspector):
--- examples.py
+++ examples.py
@@ -19,7 +19,7 @@ complex_dict = {
                     'you': {
                         'can': {
                             'never': {
-                                'imagine': [],
+                                'imagine': ['foo', 'bar', 'baz'],
                             }
                         }
                     }
@@ -36,6 +36,10 @@ simple_dict = {
 
 string = 'The quick brown fox jumps over the lazy dog'
 
+simple_list_1 = ['foo', 'bar', 'baz']
+simple_list_2 = [1, 2, 3]
+complex_list = ['foo', 'bar', 1, 2, simple_dict, complex_dict]
+
 print("String")
 ap(string)
 print("Dict (Complex)")
@@ -46,3 +50,9 @@ print("Dict (Simple)")
 ap(simple_dict)
 print("Dict (Simple, Sort keys)")
 ap(simple_dict, { 'sort_keys': True })
+print("List (Simple 1)")
+ap(simple_list_1)
+print("List (Simple 2)")
+ap(simple_list_2)
+print("List (Complex)")
+ap(complex_list)
