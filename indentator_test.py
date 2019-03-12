ed57fc40de65e0584d11495e1612f94f10ebd3d7
KY <infcurious@gmail.com>
2019-03-12 23:31:23 +0800

    First unit test!

---
+++ indentator_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from indentator import Indentator
+
+
+class TestIndentator(unittest.TestCase):
+    """
+    Unit test for indentator.
+    """
+    def test_increase_indentator(self):
+        indentator = Indentator()
+        current_indentation = indentator.indentation
+        func = lambda: self.assertEqual(indentator.indentation, current_indentation + indentator.indent_width)
+        # before increase indentation
+        self.assertEqual(indentator.indentation, current_indentation)
+        indentator.increase_indentation(func)
+        # after increase indentation
+        self.assertEqual(indentator.indentation, current_indentation)
+
+unittest.main()
