b4c3efe047d9c730d00a05ea58c42aae93df16eb
KY <infcurious@gmail.com>
2019-03-29 20:06:17 +0800

    Move colorama#init() outside of #ap for speed

--- ap/__init__.py
+++ ap/__init__.py
@@ -1,9 +1,9 @@
 from colorama import init
 from .inspector import Inspector
 
+init(autoreset = True)
 
-def ap(object, options = {}):
-    init(autoreset = True)
+def ap(object, options = {}):    
     inspector = Inspector(options)
     awesome_output = inspector.awesomize(object)
     print(awesome_output)
